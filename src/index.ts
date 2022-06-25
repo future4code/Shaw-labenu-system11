d8402bcaa96552c37c49e21dae51246ba4b8097c
Regiscleia Dias Amorim Silva <regiscleiaa@gmail.com>
2022-06-25 17:18:15 -0300

    finalizando

--- src/index.ts
+++ src/index.ts
@@ -2,45 +2,40 @@ import * as express from "express";
 import * as cors from "cors";
 import { getStudents } from "./endpoints/searchStudents";
 import { createStudent } from "./endpoints/CreateStudents";
-import createClass from "./endPoints/createClass";
-import getActiveClasses from "./endPoints/getActiveClasses";
+import { createClass } from "./endPoints/createClass";
+import { getActiveClasses } from "./endPoints/getActiveClasses";
 import { createTeacher } from "./endpoints/createTeacher";
 import selectTeacher from "./endPoints/getTeacher";
+import { updateStudentClass } from "./endpoints/editStudents";
+import { updateClassTeacher } from "./endpoints/editClassTeacher";
+import { changeClassModule } from "./endpoints/changeClassModule";
 
-
-const app = express()
+const app = express();
 app.use(express.json());
 app.use(cors());
-
-
-
-
 
 //**→ Criar turma**
 app.post("/class", createClass);
 
 //**→ Buscar turmas ativas**
-app.get("./activeclasses", getActiveClasses );
+app.get("/classes/active/:id", getActiveClasses);
 //**→ Mudar turma de módulo**
-app.put("/muduleedit/:id", )
-
+app.put("/class/:id", changeClassModule);
 
 //**→ Criar estudante**
-app.post("/studentsnew", createStudent )
+app.post("/studentsnew", createStudent);
 //**→ Buscar estudantes através do nome**
-app.get("/students", getStudents )
+app.get("/students", getStudents);
 //**→ Mudar estudante de turma**
-app.put("/studentsedit/:id")
-
-
+app.put("/student/edit/:id", updateStudentClass);
 
 //**→ Criar docente**
-app.post("/createteacher",createTeacher )
+app.post("/createteacher", createTeacher);
 //**→ Buscar todas as pessoas docentes**
-app.get("/teacher", selectTeacher )
+app.get("/teacher", selectTeacher);
 //**→ Mudar docente de turma**
-app.put("/teachersedit/:id")
+app.put("/teacher/edit/:id", updateClassTeacher);
 
 app.listen(3003, () => {
-    console.log ("Servidor rodando na porta 3003")
-}) 
+  console.log("Servidor rodando na porta 3003");
+});
--- src/types/typeClass.ts
+++ src/types/typeClass.ts
@@ -1,24 +1,29 @@
-export type Class ={    
-    Class_Name:string,
-    Initial_Date:string,
-    Final_Date:string,
-    Module:number,
-    tipo: TYPE_CLASS
-}
+import { estudantes } from "./typeStudents";
+import { teachers } from "./typeTeacher";
 
+export type Class = {
+  id?: number;
+  Class_Name: string;
+  Initial_Date: string;
+  Final_Date: string;
+  Module: number;
+  tipo: TYPE_CLASS;
+  teachers: teachers[];
+  students: estudantes[];
+};
 
 export enum TYPE_CLASS {
-    INTEGRAL = "integral",
-    NOTURNA = "noturna",
+  INTEGRAL = "INTEGRAL",
+  NOTURNO = "NOTURNO",
 }
 
 export enum Modules {
-    mod0 = 0, 
-    mod1 = 1,
-    mod2 = 2,
-    mod3 = 3,
-    mod4 = 4,
-    mod5 = 5,
-    mod6 = 6,
-    mod7 = 7
-} 
+  mod0 = 0,
+  mod1 = 1,
+  mod2 = 2,
+  mod3 = 3,
+  mod4 = 4,
+  mod5 = 5,
+  mod6 = 6,
+  mod7 = 7,
+}
